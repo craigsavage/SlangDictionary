ff6f75057d12c67d22e3d3f22cfa3288a288d8dd
Craig Savage <31861025+craigsavage@users.noreply.github.com>
2019-10-02 13:50:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,40 +11,29 @@ public class Main {
 
         System.out.println("Welcome to Slang Dictionary!");
 
-        // Initialize an ArrayList for storing new slang
-        ArrayList<Slang> slangDictionary = new ArrayList<>();
+        // Creates a new Slang Dictionary object
+        SlangDictionary dictionary = new SlangDictionary();
 
-        // Add the new slang to the dictionary (Manually to start populating dictionary)
-        slangDictionary.add(new Slang("Facts", "used to say that something is true."));
-        slangDictionary.add(new Slang("Roadman", "a young adult boy that deals drugs, smokes weed, does robberies, etc... "));
+        // Prints out a random slang from the dictionary
+        dictionary.getRandomSlang();
+
+//        // Add new user inputted slang
+//        dictionary.addItem(createSlang());
+//
+//        // Print out the entire slang dictionary
+//        dictionary.printSlangDictionary();
+    }
+
+    public static Slang createSlang() {
+        Scanner in = new Scanner(System.in);
 
         // Get new user inputted slang and add it to the dictionary
         System.out.print("Enter a new slang word: ");
         String newSlangWord = in.nextLine();
+
         System.out.print("Enter a definition for your word: ");
         String newSlangDefinition = in.nextLine();
 
-        // Add new user inputted slang
-        slangDictionary.add(new Slang(newSlangWord, newSlangDefinition));
-
-        // Print out the entire slang dictionary
-        printSlangDictionary(slangDictionary);
-    }
-
-    public void createSlang() {
-//        Scanner in = new Scanner(System.in);
-    }
-
-    /**
-     * Prints out every slang in the dictionary (so far)
-     * @param slangs used to store the current slang object to be printed.
-     */
-    public static void printSlangDictionary(ArrayList<Slang> slangs) {
-        System.out.println("\nSlang Dictionary (Updated)");
-        System.out.println("--------------------------");
-
-        for (Slang slang : slangs) {
-            System.out.println(slang);
-        }
+        return new Slang(newSlangWord, newSlangDefinition);
     }
 }
